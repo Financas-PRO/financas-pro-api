620df598e8d3b26bf3f563b4b1eb31b67811030a
eadEstagio2 <joaolucas123.ba@gmail.com>
2023-08-25 14:39:30 -0300

    feat: atualizar dados de alunos

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\Curso;
+use Illuminate\Support\Facades\Validator;
 use stdClass;
 
 class Aluno extends Model
@@ -28,34 +29,35 @@ class Aluno extends Model
         'ativo'
     ];
 
-    public function user(){
+    public function user()
+    {
         return $this->hasOne(User::class, 'id', 'id_usuario');
     }
 
-    public function curso(){
+    public function curso()
+    {
         return $this->hasOne(Curso::class, 'id', 'id_curso');
     }
 
-    /**
-     * TODO: se o aluno já está registrado no sistema, procurar os dados pelo username que vem do txt
-     * e atualizar? Como lidar com alunos que estão em mais de um curso, termo, turma...
-     */
-    public function tratarArquivoAlunos($string){
+    public function tratarArquivoAlunos($string)
+    {
 
         $alunos = [];
 
         $data_alunos = array_filter(preg_split('/\r\n|\r|\n/', $string));
 
-        foreach ($data_alunos as $data_aluno){
+        foreach ($data_alunos as $data_aluno) {
 
             $info_aluno = array_filter(explode(';', $data_aluno));
 
             $objAluno = new stdClass();
 
-            $propiedades = ['curso', 'disciplina', 'termo', 'turma', 'ano', 
-            'semestre', 'cod_disciplina', 'username', 'ra', 'nome', 'email'];
+            $propiedades = [
+                'curso', 'disciplina', 'termo', 'turma', 'ano',
+                'semestre', 'cod_disciplina', 'username', 'ra', 'nome', 'email'
+            ];
 
-            foreach ($info_aluno as $key => $prop_valor){
+            foreach ($info_aluno as $key => $prop_valor) {
 
                 $prop = $propiedades[$key];
                 $objAluno->$prop = $prop_valor;
@@ -67,36 +69,57 @@ class Aluno extends Model
         return $alunos;
     }
 
-    public function importarAlunos($alunos){
+    public function importarAlunos($alunos)
+    {
 
         $importados = [];
 
         foreach ($alunos as $aluno) {
 
-            // $disciplina = Disciplina::where('nome', $aluno->disciplina);
             $curso = DB::table('cursos')->where('curso', $aluno->curso)->first();
 
-            $model_usuario = new User(['username' => $aluno->username, 'password' => $this->gerarSenha(["nome" => $aluno->nome, "ra" => $aluno->ra]),
-            "email" => $aluno->email, 'id_tipoDeUsuario' => 3]);
-            $model_usuario->save();
+            $user = (new User())->findForPassport($aluno->username);
 
-            $model_aluno = new Aluno(['nome' => $aluno->nome, 'ra' => $aluno->ra, 'id_usuario' => $model_usuario->id, "id_curso" => $curso->id, 
-            'termo' => $aluno->termo]);
-            $model_aluno->save();
+            if (is_null($user)) {
 
-            array_push($importados, Aluno::find($model_aluno->id));
+                $model_usuario = new User([
+                    'username' => $aluno->username, 'password' => $this->gerarSenha(["nome" => $aluno->nome, "ra" => $aluno->ra]),
+                    "email" => $aluno->email, 'id_tipoDeUsuario' => 3
+                ]);
+                $model_usuario->save();
 
+                $model_aluno = new Aluno([
+                    'nome' => $aluno->nome, 'ra' => $aluno->ra, 'id_usuario' => $model_usuario->id, "id_curso" => $curso->id,
+                    'termo' => $aluno->termo
+                ]);
+                $model_aluno->save();
+
+                array_push($importados, Aluno::find($model_aluno->id));
+
+            } else {
+                array_push($importados, $this->atualizarAluno($aluno, $curso->id));
+            }
         };
 
         return $importados;
     }
 
-    private function gerarSenha($info){
-
+    private function gerarSenha($info)
+    {
         $nome = explode(" ", $info["nome"]);
         $ra = substr($info["ra"], -3);
 
         return $nome[0] . $nome[1] . "@" . $ra;
+    }
+
+    private function atualizarAluno($aluno, $cursoid){
+
+        $model_usuario = User::where("username", $aluno->username)->first();
+        $model_aluno = Aluno::where("id_usuario", $model_usuario->id)->first();
+
+        $model_aluno->update(["id_curso" => $cursoid, 'termo' => $aluno->termo]);
+
+        return $model_aluno;
 
     }
 }
