e8bbd01b58d2a043c5cf2e86725ea2158f036989
eadEstagio2 <joaolucas123.ba@gmail.com>
2023-11-01 09:43:49 -0300

    Update GrupoController.php

--- app/Http/Controllers/GrupoController.php
+++ app/Http/Controllers/GrupoController.php
@@ -104,9 +104,10 @@ class GrupoController extends Controller
         $grupo->update(["etapa" => $dados['etapa']]);
 
         foreach ($dados["acoes"] as $dadoacao){
+            dd($dadoacao->id);
             $acao = Acao::find($dadoacao->id);
 
-            $acao->update(["planilha_grupo" => $dadoacao->planilha]);
+            $acao->update(["planilha_grupo" => $dadoacao->planilha_grupo]);
         }
 
         return [
